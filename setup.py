114ed2478171885aaadbabef264b288850a58583
quaspas <daniel.quattro@gmail.com>
2014-06-10 10:21:25 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name="Python Brandstem",
     version="1.0",
-    packages=['PythonBrandStem',],
+    packages=['PythonBrandStem','PythonBrandStem.client'],
     license="MIT",
-    requires=[
-        'requests(>=1.0,<3.0)',
-    ],
-    install_requires=[
-        'requests>=1.0,<3.0',
-    ],
+    requires=['requests(>=1.0,<3.0)'],
+    install_requires=['requests>=1.0,<3.0'],
 )
