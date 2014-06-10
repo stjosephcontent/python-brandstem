fab9a94d711bee341df6d7d50f3a3ed228ec81dc
quaspas <daniel.quattro@gmail.com>
2014-06-10 09:54:03 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="Python Brandstem",
+    version="1.0",
+    packages=['PythonBrandStem',],
+    license="MIT",
+    requires=[
+        'requests(>=1.0,<3.0)',
+    ],
+    install_requires=[
+        'requests>=1.0,<3.0',
+    ],
+)
